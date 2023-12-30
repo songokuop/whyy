b1c90a83d8fdc27a2fce5c6e8079acc1a7dcc145
songokuop <149889315+songokuop@users.noreply.github.com>
2023-12-30 21:15:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/songokuop/Moviessss
+  git clone https://github.com/songokuop/Moviessss.git /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Moviessss
+  git clone $UPSTREAM_REPO /Elsa
 fi
-cd /Moviessss
+cd /Elsa
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
