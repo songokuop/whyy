c48b06a2a5af1815ea45ad4ce17d47574f4e79d8
songokuop <149889315+songokuop@users.noreply.github.com>
2024-01-20 23:40:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/songokuop/Moviessss
+  git clone https://github.com/songokuop/whyy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Moviessss
+  git clone $UPSTREAM_REPO /whyy
 fi
-cd /Moviessss
+cd /whyy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
